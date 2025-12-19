a584129a2f4431e894cdc3a1f75099393f87231b
azamdev105 <azammuhammad.1001@outlook.com>
2025-12-19 09:34:29 +0800

    Solve 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisibleBy4 = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const divisibleBy400 = year % 400 === 0;
+    return divisibleBy4 && (!isCentury || divisibleBy400);
 };
 
 // Do not edit below this line
